bd11d79b8f079f446039930c4a32490d371b09a6
Axel Mendoza <aekroft@gmail.com>
2015-12-16 00:57:57 -0500

    [NEW] module solt_qweb_widgets

---
+++ addons/solt_qweb_widgets/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Soltein QWeb Widgets',
+    'version': '0.1',
+    'category': 'tools',
+    'description': """This module brings some new OpenERP QWeb widgets.""",
+    'author': 'aekroft@gmail.com',
+    'website': 'www.soltein.org',
+    'depends': ['base', 'web', 'website'],
+    'data': [],
+    'active': False,
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
