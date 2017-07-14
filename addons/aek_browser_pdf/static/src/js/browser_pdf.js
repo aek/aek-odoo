a1e46fbd4cdcaeabfd89a060ac6f06327d35fdd9
aek <aekroft@gmail.com>
2017-07-14 15:29:32 -0500

    make it more simple

--- addons/aek_browser_pdf/static/src/js/browser_pdf.js
+++ addons/aek_browser_pdf/static/src/js/browser_pdf.js
@@ -3,13 +3,33 @@ odoo.define('aek_browser_pdf', function (require) {
     var Model = require('web.Model');
 
     function aek_browser_pdf(element, action) {
-        var ids = Array.isArray(action.params.ids) ? action.params.ids : [action.params.ids];
+        var ids = action.context.active_ids;
         new Model('report').call('get_action_html', [
-            ids, action.params.report_name, action.params.data, action.params.context
+            ids, action.report_name, action.data, action.context
         ]).then(function(res) {
             var paper_format = res.paper_format;
             printJS({printable_html:res.content, type:'html'});
         });
     };
     core.action_registry.add('aek_browser_pdf', aek_browser_pdf);
+
+    /*
+    var ReportAction = require('report.client_action');
+
+    ReportAction.include({
+        on_click_print: function () {
+            var action = {
+                'type': 'ir.actions.client',
+                'tag': 'aek_browser_pdf',
+                'report_type': 'qweb-pdf',
+                'report_name': this.report_name,
+                'report_file': this.report_file,
+                //'ids': self.ids,
+                'data': this.data,
+                'context': this.context,
+                'display_name': this.title,
+            };
+            return this.do_action(action);
+        },
+    });*/
 });
